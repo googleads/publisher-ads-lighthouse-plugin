40f9f53698fae2f660d06521e4f038484e978862
Gabriel Matute <gmatute@google.com>
2018-05-30 12:54:20 -0400

    Replace puppeteer with chrome-launcher for automatic port selection.
    Change-Id: Ic91036237d5692f5e62f8391004c975b1ad765e6

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-const auditsConfig = require('./audits/config');
 // TODO(warrengm): Add types for lighthouse.
 // @ts-ignore
+const ChromeLauncher = require('chrome-launcher');
 const lighthouse = require('lighthouse');
-const puppeteer = require('puppeteer');
 // @ts-ignore any
 const {argv} = require('yargs');
 
-const CHROME_REMOTE_PORT = 9222;
+const auditsConfig = require('./audits/config');
 
 /**
  * Flags for running lighthouse. See
  * https://github.com/GoogleChrome/lighthouse/blob/master/typings/externs.d.ts
- * @const {!LH.Flags}
+ * @type {!LH.Flags}
  */
-const flags = {
+ const flags = {
   json: true,
   runs: 1,
-  port: CHROME_REMOTE_PORT,
 };
 
 async function main() {
   try {
-    const browser = await puppeteer.launch({
-      args: [`--remote-debugging-port=${CHROME_REMOTE_PORT}`],
-    });
+    const browser = await ChromeLauncher.launch({chromeFlags: ['--headless']});
     console.log('Launched headless chrome');
+    flags.port = browser.port;
+
     const results = await lighthouse(argv.url, flags, auditsConfig);
-    await browser.close();
+    await browser.kill();
+
     for (const {name, displayValue} of Object.values(results.audits)) {
       if (displayValue) {
         console.log(name, ':', displayValue);
