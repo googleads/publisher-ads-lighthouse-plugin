0167940da3ecafa6bb56cfb6cb19f176f8eb3cca
Gabriel Matute <gmatute@google.com>
2018-06-25 11:00:02 -0400

    Refactor AdsInViewport tests to use tables.
    Change-Id: Ic64207b233abcee04761e7b46bd37a7cf2d3d7ac

--- test/audits/ads-in-viewport_test.js
+++ test/audits/ads-in-viewport_test.js
@@ -2,16 +2,16 @@ const AdsInViewport = require('../../audits/ads-in-viewport');
 const {expect} = require('chai');
 
 describe('AdsInViewport', () => {
-  // Quad from top left corner (x, y) and dimensions (w, h)
-  const generateQuad = ({x, y, w, h}) => [x, y, x+w, y, x+w, y+h, x, y+h];
+  // From top left corner & dimensions
+  const generateSlot = ({x, y, w, h}) =>
+    ({content: [x, y, x+w, y, x+w, y+h, x, y+h]});
+
+  const ViewportDimensions = {
+    innerHeight: 200,
+    innerWidth: 300,
+  };
 
   describe('rawValue', () => {
-    const ViewportDimensions = {
-      // Most desktops size
-      innerWidth: 1000,
-      innerHeight: 700,
-    };
-
     it('should succeed if there are no ad slots', async () => {
       const RenderedAdSlots = [];
 
@@ -19,18 +19,11 @@ describe('AdsInViewport', () => {
       expect(AdsInViewport.audit(artifacts)).to.have.property('rawValue', true);
     });
 
-    it('should return zero if all ad slots are hidden', async () => {
-      const RenderedAdSlots = [null, null, null];
-
-      const artifacts = {RenderedAdSlots, ViewportDimensions};
-      expect(AdsInViewport.audit(artifacts)).to.have.property('rawValue', 0);
-    });
-
     it('should return fraction of ad slots inside viewport', async () => {
       const RenderedAdSlots = [
-        {content: generateQuad({x: 0, y: 100, w: 50, h: 50})}, // in
-        {content: generateQuad({x: 0, y: 700, w: 50, h: 50})}, // out
-        {content: generateQuad({x: 0, y: 800, w: 50, h: 50})}, // out
+        generateSlot({x: 0, y: 100, w: 50, h: 50}), // in
+        generateSlot({x: 0, y: 700, w: 50, h: 50}), // out
+        generateSlot({x: 0, y: 800, w: 50, h: 50}), // out
         null, null, // hidden
       ];
 
@@ -38,67 +31,57 @@ describe('AdsInViewport', () => {
       expect(AdsInViewport.audit(artifacts)).to.have.property('rawValue', 1/5);
     });
 
-    it('should handle slots on an edge of the viewport as seen', async () => {
-      const RenderedAdSlots = [
-        {content: generateQuad({x: 10, y: -10, w: 900, h: 50})},
-        {content: generateQuad({x: -10, y: 10, w: 50, h: 900})},
-      ];
+    const positiveTests = [
+      {x: 10, y: 10, w: 10, h: 10, overlap: 100, pos: 'inside'},
+      // Cases overlapping an edge of the viewport
+      {x: -10, y: 0, w: 20, h: 200, overlap: 2000, pos: 'left edge'},
+      {x: 0, y: -10, w: 300, h: 20, overlap: 3000, pos: 'top edge'},
+      {x: 290, y: 0, w: 20, h: 200, overlap: 2000, pos: 'right edge'},
+      {x: 0, y: 190, w: 300, h: 20, overlap: 3000, pos: 'bottom edge'},
+      // Cases overlapping a corner of the viewport
+      {x: -10, y: -10, w: 20, h: 20, overlap: 100, pos: 'top left corner'},
+      {x: 290, y: -10, w: 20, h: 20, overlap: 100, pos: 'top right corner'},
+      {x: -10, y: 190, w: 20, h: 20, overlap: 100, pos: 'bottom left corner'},
+      {x: 290, y: 190, w: 20, h: 20, overlap: 100, pos: 'bottom right corner'},
+    ];
 
-      const artifacts = {RenderedAdSlots, ViewportDimensions};
-      expect(AdsInViewport.audit(artifacts)).to.have.property('rawValue', 1);
-    });
+    positiveTests.forEach(async (test) =>
+      it(`should handle slots on the ${test.pos} properly`, () => {
+        const RenderedAdSlots = [generateSlot(test)];
+        const artifacts = {RenderedAdSlots, ViewportDimensions};
+        expect(AdsInViewport.audit(artifacts)).to.have.property('rawValue', 1);
+      })
+    );
 
-    it('should handle slots on a corner of the viewport as seen', async () => {
-      const RenderedAdSlots = [
-        {content: generateQuad({x: -10, y: -10, w: 50, h: 50})},
-        {content: generateQuad({x: 990, y: 690, w: 50, h: 50})},
-      ];
+    const negativeTests = [
+      // Cases outside an edge of the viewport
+      {x: -20, y: 0, w: 20, h: 200, pos: 'left edge'},
+      {x: 0, y: -20, w: 300, h: 20, pos: 'top edge'},
+      {x: 300, y: 0, w: 20, h: 200, pos: 'right edge'},
+      {x: 0, y: 200, w: 300, h: 20, pos: 'bottom edge'},
+      // Cases outside a corner of the viewport
+      {x: -20, y: -20, w: 20, h: 20, pos: 'top left corner'},
+      {x: 300, y: -20, w: 20, h: 20, pos: 'top right corner'},
+      {x: -20, y: 200, w: 20, h: 20, pos: 'bottom left corner'},
+      {x: 300, y: 200, w: 20, h: 20, pos: 'bottom right corner'},
+    ];
 
-      const artifacts = {RenderedAdSlots, ViewportDimensions};
-      expect(AdsInViewport.audit(artifacts)).to.have.property('rawValue', 1);
-    });
+    negativeTests.forEach(async (test) =>
+      it(`should handle slots outside the ${test.pos}`, () => {
+        const RenderedAdSlots = [generateSlot(test)];
+        const artifacts = {RenderedAdSlots, ViewportDimensions};
+        expect(AdsInViewport.audit(artifacts)).to.have.property('rawValue', 0);
+      })
+    );
 
-    it('should handle slots left of the viewport as unseen', async () => {
-      const RenderedAdSlots = [
-        {content: generateQuad({x: -10, y: 0, w: 10, h: 700})},
-      ];
-
-      const artifacts = {RenderedAdSlots, ViewportDimensions};
-      expect(AdsInViewport.audit(artifacts)).to.have.property('rawValue', 0);
-    });
-
-    it('should handle slots above the viewport as unseen', async () => {
-      const RenderedAdSlots = [
-        {content: generateQuad({x: 0, y: -10, w: 1000, h: 10})},
-      ];
-
-      const artifacts = {RenderedAdSlots, ViewportDimensions};
-      expect(AdsInViewport.audit(artifacts)).to.have.property('rawValue', 0);
-    });
-
-    it('should handle slots right of the viewport as unseen', async () => {
-      const RenderedAdSlots = [
-        {content: generateQuad({x: 1000, y: 0, w: 10, h: 700})},
-      ];
-
-      const artifacts = {RenderedAdSlots, ViewportDimensions};
-      expect(AdsInViewport.audit(artifacts)).to.have.property('rawValue', 0);
-    });
-
-    it('should handle slots below the viewport as unseen', async () => {
-      const RenderedAdSlots = [
-        {content: generateQuad({x: 0, y: 700, w: 1000, h: 10})},
-      ];
-
+    it('should handle slots that have no area', async () => {
+      const RenderedAdSlots = [generateSlot({x: 10, y: 10, w: 0, h: 0})];
       const artifacts = {RenderedAdSlots, ViewportDimensions};
       expect(AdsInViewport.audit(artifacts)).to.have.property('rawValue', 0);
     });
 
-    it('should handle slots with no area as unseen', async () => {
-      const RenderedAdSlots = [
-        {content: generateQuad({x: 10, y: 10, w: 0, h: 0})},
-      ];
-
+    it('should handle slots that are hidden', async () => {
+      const RenderedAdSlots = [null]; // no box model if slot is hidden
       const artifacts = {RenderedAdSlots, ViewportDimensions};
       expect(AdsInViewport.audit(artifacts)).to.have.property('rawValue', 0);
     });
