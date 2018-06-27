524d78021e4303c1a598915e6e90b6d729c8a69e
Gabriel Matute <gmatute@google.com>
2018-06-27 11:41:34 -0400

    Separate LH types from our custom extensions.
    Change-Id: Ia0cc3df16ad765213f4836cbd79854c2135f9b6f

---
+++ typings/artifacts.d.ts
@@ -0,0 +1,15 @@
+import './lh-externs';
+
+declare global {
+  export interface Artifacts extends LH.Artifacts {
+    RenderedAdSlots?: Array<LH.Crdp.DOM.BoxModel | null>;
+    StaticAdTags?: Array<LH.Crdp.DOM.Node>;
+    Network?: NetworkArtifacts;
+  }
+
+  export interface NetworkArtifacts {
+    har: HAR.Har;
+    networkRecords: Array<LH.WebInspector.NetworkRequest>;
+    parsedUrls: Array<URL>;
+  }
+}
