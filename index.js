39e80dae36fe9066f68fc1decaa5faaf866a1e88
Lucas Silva <lusilva@google.com>
2018-06-28 16:05:40 -0400

    Add new flags --output-path and --view.
    It writes the lighthouse output to either a JSON or HTML file, depending
    on the output-path flag.
    
    Change-Id: Ifffb107d91badc7fe56ca2db34e0d457217bd404

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const util = require('util');
 
 // @ts-ignore
 const ChromeLauncher = require('chrome-launcher');
+const fs = require('fs-extra');
 const lighthouse = require('lighthouse');
+const opn = require('opn');
 // @ts-ignore any
 const {argv} = require('yargs');
 
@@ -24,6 +26,15 @@ async function main() {
     const browser = await ChromeLauncher.launch({chromeFlags: ['--headless']});
     console.log('Launched headless chrome');
     flags.port = browser.port;
+    const outputPath = argv.outputPath || '';
+    if (outputPath.endsWith('.json')) {
+      flags.output = 'json';
+    } else if (outputPath.endsWith('.html')) {
+      flags.output = 'html';
+    } else if (outputPath) {
+      throw new Error(`output file must be either a .html or .json file. ` +
+        `Was ${outputPath}`);
+    }
 
     const results = await lighthouse(argv.url, flags, config);
     await browser.kill();
@@ -40,6 +51,14 @@ async function main() {
         const display = typeof displayValue == 'string' ?
           displayValue : util.format.call(null, ...displayValue);
         console.log(id, ':', display);
+      }
+    }
+
+    if (outputPath) {
+      console.log(`Writing output to ${outputPath}`);
+      await fs.outputPath(outputPath, results.report);
+      if (argv.view) {
+        opn(outputPath);
       }
     }
     process.exit(0);
