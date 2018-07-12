274beaaddb1735c7594044c1b1bcf3f91ebdb570
Benjamin Catarevas <bencatarevas@google.com>
2018-07-12 15:02:35 -0400

    Modify array util.
    Change-Id: I71e74e1442236892d62992111184a32c36b25131

--- test/utils/array_test.js
+++ test/utils/array_test.js
@@ -1,5 +1,7 @@
 const array = require('../../utils/array');
 const {expect} = require('chai');
+const {isGoogleAds} = require('../../utils/resource-classification');
+const {URL} = require('url');
 
 describe('array', () => {
   describe('#count', () => {
@@ -11,6 +13,84 @@ describe('array', () => {
     it('should handle non-empty arrays', () => {
       const num = array.count([0, 10, 0, 10, 0], (n) => n >= 10);
       expect(num).to.equal(2);
+    });
+  });
+
+  describe('#bucket', () => {
+    const RequestType = {
+      AD: 'ad',
+      UNKNOWN: 'unknown',
+    };
+
+    /**
+     * Checks the type of record.
+     * @param {NetworkDetails.RequestRecord} record
+     * @return {string}
+     */
+    function checkRecordType(record) {
+      if (isGoogleAds(new URL(record.url))) {
+        return RequestType.AD;
+      } else {
+        return RequestType.UNKNOWN;
+      }
+    }
+
+    it('should handle empty arrays', () => {
+      const records = [];
+      const results = array.bucket(records, checkRecordType);
+      expect(results).to.eql({});
+    });
+
+    it('should handle non-empty arrays', () => {
+      const records = [
+        {
+          startTime: 10,
+          endTime: 20,
+          url: 'https://example.com',
+        },
+        {
+          startTime: 15,
+          endTime: 30,
+          url: 'https://foo.uk.com',
+        },
+        {
+          startTime: 65,
+          endTime: 70,
+          url: 'https://securepubads.g.doubleclick.net/gampad/ads?bar',
+        },
+        {
+          startTime: 75,
+          endTime: 80,
+          url: 'https://googlesyndication.com/gpt/',
+        },
+      ];
+      const results = array.bucket(records, checkRecordType);
+      expect(results).to.eql({
+        'ad': [
+          {
+            startTime: 65,
+            endTime: 70,
+            url: 'https://securepubads.g.doubleclick.net/gampad/ads?bar',
+          },
+          {
+            startTime: 75,
+            endTime: 80,
+            url: 'https://googlesyndication.com/gpt/',
+          },
+        ],
+        'unknown': [
+          {
+            startTime: 10,
+            endTime: 20,
+            url: 'https://example.com',
+          },
+          {
+            startTime: 15,
+            endTime: 30,
+            url: 'https://foo.uk.com',
+          },
+        ],
+      });
     });
   });
 });
--- utils/array.js
+++ utils/array.js
@@ -1,4 +1,3 @@
-
 /**
  * Counts how many elements of the array match the given predicate.
  * @param {Array<T>} arr
@@ -16,6 +15,28 @@ function count(arr, pred) {
   return num;
 }
 
+/**
+ * Sorts an array into buckets depending on a splitting function.
+ * We use the type 'any' because the bucket function works on any type, and
+ * returns an object of arrays of any type. The type is not specified.
+ * @param {Array<any>} array
+ * @param {function(any): any} splitter
+ * @return {Object<string, Array<any>>}
+ */
+function bucket(array, splitter) {
+  const buckets = {};
+  for (const element of array) {
+    const result = splitter(element);
+    if (result) {
+      // The result is not undefined, so either push the result to the right
+      // bucket, or create it if necessary.
+      const bucket = buckets[result] || (buckets[result] = []);
+      bucket.push(element);
+    }
+  }
+  return buckets;
+}
+
 module.exports = {
-  count,
+  count, bucket,
 };
