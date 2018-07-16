9442717b608ce4f5212366f153e574757726c785
Benjamin Catarevas <bencatarevas@google.com>
2018-07-16 15:01:28 -0400

    Use parametric testing to shorten unit tests.
    Change-Id: I977bf1d0791eb0b8e75b893143bf85bc10462860

--- test/audits/loads-gpt-over-https_test.js
+++ test/audits/loads-gpt-over-https_test.js
@@ -25,258 +25,183 @@ describe('LoadsGptOverHttps', () => {
   });
 
   describe('numGptHttpReqs', () => {
-    it('should have a score of 1 for empty logs and 0 reqs', async () => {
-      const networkRecords = [];
-      sandbox.stub(chromeDriver, 'harFromMessages')
-        .returns(newHar(networkRecords));
+    const testCases = [
+      {
+        networkRecords: [],
+        expectedScore: 1,
+        expectedNumGptHttpReqs: 0,
+        expectedNumGptHttpsReqs: 0,
+      },
+      {
+        networkRecords: [
+          {url: 'http://example.com'},
+          {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
+          {url: 'https://www.googletagservices.com/tag/js/gpt.js'},
+        ],
+        expectedScore: 1,
+        expectedNumGptHttpReqs: 0,
+        expectedNumGptHttpsReqs: 1,
+      },
+      {
+        networkRecords: [
+          {url: 'http://example.com'},
+          {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
+          {url: 'https://www.googletagservices.com/tag/js/gpt.js'},
+          {url: 'http://www.googletagservices.com/tag/js/gpt.js#foo'},
+        ],
+        expectedScore: 0,
+        expectedNumGptHttpReqs: 1,
+        expectedNumGptHttpsReqs: 1,
+      },
+      {
+        networkRecords: [
+          {url: 'http://example.com'},
+          {url: 'https://securepubads.g.doubleclick.net/gpt/js/pubads.js'},
+          {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
+          {url: 'http://www.googletagservices.com/tag/js/gpt.js'},
+        ],
+        expectedScore: 0,
+        expectedNumGptHttpReqs: 1,
+        expectedNumGptHttpsReqs: 0,
+      },
+      {
+        networkRecords: [
+          {url: 'http://example.com'},
+          {url: 'https://securepubads.g.doubleclick.net/gpt/js/pubads.js'},
+          {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
+          {url: 'http://www.googletagservices.com/tag/js/gpt.js'},
+          {url: 'http://www.googletagservices.com/tag/js/gpt.js#foo'},
+          {url: 'https://www.googletagservices.com/tag/js/gpt.js'},
+        ],
+        expectedScore: 0,
+        expectedNumGptHttpReqs: 2,
+        expectedNumGptHttpsReqs: 1,
+      },
+      {
+        networkRecords: [
+          {url: 'http://example.com'},
+          {url: 'https://facebook.com/foo?bar=baz'},
+          {url: 'https://securepubads.g.doubleclick.net/gpt/js/pubads.js'},
+          {url: 'http://securepubads.g.doubleclick.net/gampad/ads?foo'},
+          {url: 'http://www.googletagservices.com/tag/js/gpt.js?foo=bar'},
+          {url: 'http://www.googletagservices.com/tag/js/gpt.js?cb=true'},
+          {url: 'http://www.googletagservices.com/tag/js/gpt.js?foo=bar#baz'},
+        ],
+        expectedScore: 0,
+        expectedNumGptHttpReqs: 3,
+        expectedNumGptHttpsReqs: 0,
+      },
+    ];
+    for (const {networkRecords, expectedScore,
+      expectedNumGptHttpReqs, expectedNumGptHttpsReqs} of testCases) {
+      it(`should have a score of ${expectedScore} with` +
+        ` ${expectedNumGptHttpReqs} HTTP requests and` +
+        ` ${expectedNumGptHttpsReqs} HTTPS requests`, () => {
+        sandbox.stub(chromeDriver, 'harFromMessages')
+          .returns(newHar(networkRecords));
 
-      const parsedUrls = networkRecords.map((request) => new URL(request.url));
-      const results = LoadsGptOverHttps.audit(
-        {Network: {har: newHar(networkRecords), parsedUrls}});
+        const parsedUrls = networkRecords.map((request) =>
+          new URL(request.url));
+        const results = LoadsGptOverHttps.audit(
+          {Network: {har: newHar(networkRecords), parsedUrls}});
 
-      expect(results).to.have.property('score', 1);
-      expect(results).with.property('details').
-        property('numGptHttpReqs').equal(0);
-      expect(results).with.property('details').
-        property('numGptHttpsReqs').equal(0);
-    });
-
-    it('should have score of 1, 0 http reqs, 1 https req', async () => {
-      const networkRecords = [
-        {url: 'http://example.com'},
-        {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
-        {url: 'https://www.googletagservices.com/tag/js/gpt.js'},
-      ];
-      sandbox.stub(chromeDriver, 'harFromMessages')
-        .returns(newHar(networkRecords));
-
-      const parsedUrls = networkRecords.map((request) => new URL(request.url));
-      const results = LoadsGptOverHttps.audit(
-        {Network: {har: newHar(networkRecords), parsedUrls}});
-
-      expect(results).to.have.property('score', 1);
-      expect(results).with.property('details').
-        property('numGptHttpReqs').equal(0);
-      expect(results).with.property('details').
-        property('numGptHttpsReqs').equal(1);
-    });
-
-    it('should have score of 0, 1 http req, and 1 https req', async () => {
-      const networkRecords = [
-        {url: 'http://example.com'},
-        {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
-        {url: 'https://www.googletagservices.com/tag/js/gpt.js'},
-        {url: 'http://www.googletagservices.com/tag/js/gpt.js#foo'},
-      ];
-      sandbox.stub(chromeDriver, 'harFromMessages')
-        .returns(newHar(networkRecords));
-
-      const parsedUrls = networkRecords.map((request) => new URL(request.url));
-      const results = LoadsGptOverHttps.audit(
-        {Network: {har: newHar(networkRecords), parsedUrls}});
-
-      expect(results).to.have.property('score', 0);
-      expect(results).with.property('details').
-        property('numGptHttpReqs').equal(1);
-      expect(results).with.property('details').
-        property('numGptHttpsReqs').equal(1);
-    });
-
-    it('should have score of 0, 1 http req, and 0 https reqs', async () => {
-      const networkRecords = [
-        {url: 'http://example.com'},
-        {url: 'https://securepubads.g.doubleclick.net/gpt/js/pubads.js'},
-        {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
-        {url: 'http://www.googletagservices.com/tag/js/gpt.js'},
-      ];
-      sandbox.stub(chromeDriver, 'harFromMessages')
-        .returns(newHar(networkRecords));
-
-      const parsedUrls = networkRecords.map((request) => new URL(request.url));
-      const results = LoadsGptOverHttps.audit(
-        {Network: {har: newHar(networkRecords), parsedUrls}});
-
-      expect(results).to.have.property('score', 0);
-      expect(results).with.property('details').
-        property('numGptHttpReqs').equal(1);
-      expect(results).with.property('details').
-        property('numGptHttpsReqs').equal(0);
-    });
-
-
-    it('should have score of 0, 2 http reqs, and 1 https reqs', async () => {
-      const networkRecords = [
-        {url: 'http://example.com'},
-        {url: 'https://securepubads.g.doubleclick.net/gpt/js/pubads.js'},
-        {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
-        {url: 'http://www.googletagservices.com/tag/js/gpt.js'},
-        {url: 'http://www.googletagservices.com/tag/js/gpt.js#foo'},
-        {url: 'https://www.googletagservices.com/tag/js/gpt.js'},
-      ];
-      sandbox.stub(chromeDriver, 'harFromMessages')
-        .returns(newHar(networkRecords));
-
-      const parsedUrls = networkRecords.map((request) => new URL(request.url));
-      const results = LoadsGptOverHttps.audit(
-        {Network: {har: newHar(networkRecords), parsedUrls}});
-
-      expect(results).to.have.property('score', 0);
-      expect(results).with.property('details').
-        property('numGptHttpReqs').equal(2);
-      expect(results).with.property('details').
-        property('numGptHttpsReqs').equal(1);
-    });
-
-    it('should have score of 0, 3 http reqs, and 0 https reqs', async () => {
-      const networkRecords = [
-        {url: 'http://example.com'},
-        {url: 'https://facebook.com/foo?bar=baz'},
-        {url: 'https://securepubads.g.doubleclick.net/gpt/js/pubads.js'},
-        {url: 'http://securepubads.g.doubleclick.net/gampad/ads?foo'},
-        {url: 'http://www.googletagservices.com/tag/js/gpt.js?foo=bar'},
-        {url: 'http://www.googletagservices.com/tag/js/gpt.js?cb=true'},
-        {url: 'http://www.googletagservices.com/tag/js/gpt.js?foo=bar#baz'},
-      ];
-      sandbox.stub(chromeDriver, 'harFromMessages')
-        .returns(newHar(networkRecords));
-
-      const parsedUrls = networkRecords.map((request) => new URL(request.url));
-      const results = LoadsGptOverHttps.audit(
-        {Network: {har: newHar(networkRecords), parsedUrls}});
-
-      expect(results).to.have.property('score', 0);
-      expect(results).with.property('details').
-        property('numGptHttpReqs').equal(3);
-      expect(results).with.property('details').
-        property('numGptHttpsReqs').equal(0);
-    });
+        expect(results).to.have.property('score', expectedScore);
+        expect(results).with.property('details')
+          .property('numGptHttpReqs').equal(expectedNumGptHttpReqs);
+        expect(results).with.property('details')
+          .property('numGptHttpsReqs').equal(expectedNumGptHttpsReqs);
+      });
+    }
   });
 
   describe('numGptHttpsReqs', async () => {
-    it('should have a score of 1 for empty logs and 0 reqs', async () => {
-      const networkRecords = [];
-      sandbox.stub(chromeDriver, 'harFromMessages')
-        .returns(newHar(networkRecords));
+    const testCases = [
+      {
+        networkRecords: [],
+        expectedScore: 1,
+        expectedNumGptHttpReqs: 0,
+        expectedNumGptHttpsReqs: 0,
+      },
+      {
+        networkRecords: [
+          {url: 'http://example.com'},
+          {url: 'https://securepubads.g.doubleclick.net/gpt/js/pubads.js'},
+          {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
+          {url: 'http://www.googletagservices.com/tag/js/gpt.js'},
+        ],
+        expectedScore: 0,
+        expectedNumGptHttpReqs: 1,
+        expectedNumGptHttpsReqs: 0,
+      },
+      {
+        networkRecords: [
+          {url: 'http://example.com'},
+          {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
+          {url: 'https://www.googletagservices.com/tag/js/gpt.js'},
+          {url: 'https://www.googletagservices.com/tag/js/gpt.js?foo=bar'},
+          {url: 'http://www.googletagservices.com/tag/js/gpt.js#foo'},
+        ],
+        expectedScore: 0,
+        expectedNumGptHttpReqs: 1,
+        expectedNumGptHttpsReqs: 2,
+      },
+      {
+        networkRecords: [
+          {url: 'http://example.com'},
+          {url: 'https://securepubads.g.doubleclick.net/gpt/js/pubads.js'},
+          {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
+          {url: 'https://www.googletagservices.com/tag/js/gpt.js'},
+        ],
+        expectedScore: 1,
+        expectedNumGptHttpReqs: 0,
+        expectedNumGptHttpsReqs: 1,
+      },
+      {
+        networkRecords: [
+          {url: 'http://example.com'},
+          {url: 'https://securepubads.g.doubleclick.net/gpt/js/pubads.js'},
+          {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
+          {url: 'https://www.googletagservices.com/tag/js/gpt.js'},
+          {url: 'https://www.googletagservices.com/tag/js/gpt.js#foo'},
+        ],
+        expectedScore: 1,
+        expectedNumGptHttpReqs: 0,
+        expectedNumGptHttpsReqs: 2,
+      },
+      {
+        networkRecords: [
+          {url: 'http://example.com'},
+          {url: 'https://facebook.com/foo?bar=baz'},
+          {url: 'https://securepubads.g.doubleclick.net/gpt/js/pubads.js'},
+          {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
+          {url: 'https://www.googletagservices.com/tag/js/gpt.js?foo=bar'},
+          {url: 'https://www.googletagservices.com/tag/js/gpt.js?cb=true'},
+          {url: 'https://www.googletagservices.com/tag/js/gpt.js?foo=bar#baz'},
+        ],
+        expectedScore: 1,
+        expectedNumGptHttpReqs: 0,
+        expectedNumGptHttpsReqs: 3,
+      },
+    ];
+    for (const {networkRecords, expectedScore,
+      expectedNumGptHttpReqs, expectedNumGptHttpsReqs} of testCases) {
+      it(`should have a score of ${expectedScore} with` +
+        ` ${expectedNumGptHttpReqs} HTTP requests and` +
+        ` ${expectedNumGptHttpsReqs} HTTPS requests`, () => {
+        sandbox.stub(chromeDriver, 'harFromMessages')
+          .returns(newHar(networkRecords));
 
-      const parsedUrls = networkRecords.map((request) => new URL(request.url));
-      const results = LoadsGptOverHttps.audit(
-        {Network: {har: newHar(networkRecords), parsedUrls}});
+        const parsedUrls = networkRecords.map((request) =>
+          new URL(request.url));
+        const results = LoadsGptOverHttps.audit(
+          {Network: {har: newHar(networkRecords), parsedUrls}});
 
-      expect(results).to.have.property('score', 1);
-      expect(results).with.property('details').
-        property('numGptHttpReqs').equal(0);
-      expect(results).with.property('details').
-        property('numGptHttpsReqs').equal(0);
-    });
-
-    it('should have a score of 0, 1 http req, 0 https reqs', async () => {
-      const networkRecords = [
-        {url: 'http://example.com'},
-        {url: 'https://securepubads.g.doubleclick.net/gpt/js/pubads.js'},
-        {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
-        {url: 'http://www.googletagservices.com/tag/js/gpt.js'},
-      ];
-      sandbox.stub(chromeDriver, 'harFromMessages')
-        .returns(newHar(networkRecords));
-
-      const parsedUrls = networkRecords.map((request) => new URL(request.url));
-      const results = LoadsGptOverHttps.audit(
-        {Network: {har: newHar(networkRecords), parsedUrls}});
-
-      expect(results).to.have.property('score', 0);
-      expect(results).with.property('details').
-        property('numGptHttpReqs').equal(1);
-      expect(results).with.property('details').
-        property('numGptHttpsReqs').equal(0);
-    });
-
-    it('should have a score of 0, 1 http req, 2 https req', async () => {
-      const networkRecords = [
-        {url: 'http://example.com'},
-        {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
-        {url: 'https://www.googletagservices.com/tag/js/gpt.js'},
-        {url: 'https://www.googletagservices.com/tag/js/gpt.js?foo=bar'},
-        {url: 'http://www.googletagservices.com/tag/js/gpt.js#foo'},
-      ];
-      sandbox.stub(chromeDriver, 'harFromMessages')
-        .returns(newHar(networkRecords));
-
-      const parsedUrls = networkRecords.map((request) => new URL(request.url));
-      const results = LoadsGptOverHttps.audit(
-        {Network: {har: newHar(networkRecords), parsedUrls}});
-
-      expect(results).to.have.property('score', 0);
-      expect(results).with.property('details').
-        property('numGptHttpReqs').equal(1);
-      expect(results).with.property('details').
-        property('numGptHttpsReqs').equal(2);
-    });
-
-    it('should have a score of 1, 0 http req, 1 https req', async () => {
-      const networkRecords = [
-        {url: 'http://example.com'},
-        {url: 'https://securepubads.g.doubleclick.net/gpt/js/pubads.js'},
-        {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
-        {url: 'https://www.googletagservices.com/tag/js/gpt.js'},
-      ];
-      sandbox.stub(chromeDriver, 'harFromMessages')
-        .returns(newHar(networkRecords));
-
-      const parsedUrls = networkRecords.map((request) => new URL(request.url));
-      const results = LoadsGptOverHttps.audit(
-        {Network: {har: newHar(networkRecords), parsedUrls}});
-
-      expect(results).to.have.property('score', 1);
-      expect(results).with.property('details').
-        property('numGptHttpReqs').equal(0);
-      expect(results).with.property('details').
-        property('numGptHttpsReqs').equal(1);
-    });
-
-    it('should have a score of 1, 0 http reqs, 2 https req', async () => {
-      const networkRecords = [
-        {url: 'http://example.com'},
-        {url: 'https://securepubads.g.doubleclick.net/gpt/js/pubads.js'},
-        {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
-        {url: 'https://www.googletagservices.com/tag/js/gpt.js'},
-        {url: 'https://www.googletagservices.com/tag/js/gpt.js#foo'},
-      ];
-      sandbox.stub(chromeDriver, 'harFromMessages')
-        .returns(newHar(networkRecords));
-
-      const parsedUrls = networkRecords.map((request) => new URL(request.url));
-      const results = LoadsGptOverHttps.audit(
-        {Network: {har: newHar(networkRecords), parsedUrls}});
-
-      expect(results).to.have.property('score', 1);
-      expect(results).with.property('details').
-        property('numGptHttpReqs').equal(0);
-      expect(results).with.property('details').
-        property('numGptHttpsReqs').equal(2);
-    });
-
-    it('should have a score of 1, 0 http reqs, 3 https req', async () => {
-      const networkRecords = [
-        {url: 'http://example.com'},
-        {url: 'https://facebook.com/foo?bar=baz'},
-        {url: 'https://securepubads.g.doubleclick.net/gpt/js/pubads.js'},
-        {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
-        {url: 'https://www.googletagservices.com/tag/js/gpt.js?foo=bar'},
-        {url: 'https://www.googletagservices.com/tag/js/gpt.js?cb=true'},
-        {url: 'https://www.googletagservices.com/tag/js/gpt.js?foo=bar#baz'},
-      ];
-      sandbox.stub(chromeDriver, 'harFromMessages')
-        .returns(newHar(networkRecords));
-
-      const parsedUrls = networkRecords.map((request) => new URL(request.url));
-      const results = LoadsGptOverHttps.audit(
-        {Network: {har: newHar(networkRecords), parsedUrls}});
-
-      expect(results).to.have.property('score', 1);
-      expect(results).with.property('details').
-        property('numGptHttpReqs').equal(0);
-      expect(results).with.property('details').
-        property('numGptHttpsReqs').equal(3);
-    });
+        expect(results).to.have.property('score', expectedScore);
+        expect(results).with.property('details')
+          .property('numGptHttpReqs').equal(expectedNumGptHttpReqs);
+        expect(results).with.property('details')
+          .property('numGptHttpsReqs').equal(expectedNumGptHttpsReqs);
+      });
+    }
   });
 });
