91063dcc745ffc1885dc730ee18c07a8e38bd6b3
Benjamin Catarevas <bencatarevas@google.com>
2018-07-16 15:01:28 -0400

    Use parametric testing to shorten unit tests.
    Change-Id: I977bf1d0791eb0b8e75b893143bf85bc10462860

--- test/audits/has-ads_test.js
+++ test/audits/has-ads_test.js
@@ -25,157 +25,130 @@ describe('HasAds', () => {
   });
 
   describe('numRequests', () => {
-    it('should have a score of 0 for empty logs and 0 reqs', async () => {
-      const networkRecords = [];
-      sandbox.stub(chromeDriver, 'harFromMessages')
-        .returns(newHar(networkRecords));
+    const testCases = [
+      {
+        description: 'empty logs',
+        networkRecords: [],
+        expectedScore: 0,
+        expectedNumRequests: 0,
+      },
+      {
+        description: 'logs without requests',
+        networkRecords: [
+          {url: 'http://example.com'},
+          {url: 'https://securepubads.g.doubleclick.net/gpt/js/pubads.js'},
+        ],
+        expectedScore: 0,
+        expectedNumRequests: 0,
+      },
+      {
+        description: 'logs with single request',
+        networkRecords: [
+          {url: 'http://example.com'},
+          {url: 'https://securepubads.g.doubleclick.net/gpt/js/pubads.js'},
+          {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
+        ],
+        expectedScore: 1,
+        expectedNumRequests: 1,
+      },
+      {
+        description: 'log with only one request record',
+        networkRecords: [
+          {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
+        ],
+        expectedScore: 1,
+        expectedNumRequests: 1,
+      },
+      {
+        description: 'empty logs',
+        networkRecords: [
+          {url: 'http://example.com'},
+          {url: 'https://securepubads.g.doubleclick.net/gpt/js/pubads.js'},
+          {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
+          {url: 'https://securepubads.g.doubleclick.net/other'},
+          {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
+          {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
+        ],
+        expectedScore: 1,
+        expectedNumRequests: 3,
+      },
+    ];
+    for (const {description, networkRecords,
+      expectedScore, expectedNumRequests} of testCases) {
+      it(`should have score of ${expectedScore} for ${description} with` +
+        ` ${expectedNumRequests} requests`, () => {
+        sandbox.stub(chromeDriver, 'harFromMessages')
+          .returns(newHar(networkRecords));
 
-      const parsedUrls = networkRecords.map((request) => new URL(request.url));
-      const results = HasAds.audit(
-        {Network: {har: newHar(networkRecords), parsedUrls}});
+        const parsedUrls = networkRecords.map((request) =>
+          new URL(request.url));
+        const results = HasAds.audit(
+          {Network: {har: newHar(networkRecords), parsedUrls}});
 
-      expect(results).with.property('score', 0);
-      expect(results).with.property('details').property('numRequests').equal(0);
-    });
-
-    it('should have a score of 0 for no reqs logs and 0 reqs', async () => {
-      const networkRecords = [
-        {url: 'http://example.com'},
-        {url: 'https://securepubads.g.doubleclick.net/gpt/js/pubads.js'},
-      ];
-      sandbox.stub(chromeDriver, 'harFromMessages')
-        .returns(newHar(networkRecords));
-
-      const parsedUrls = networkRecords.map((request) => new URL(request.url));
-      const results = HasAds.audit(
-        {Network: {har: newHar(networkRecords), parsedUrls}});
-
-      expect(results).with.property('score', 0);
-      expect(results).with.property('details').property('numRequests').equal(0);
-    });
-
-    it('should have a score of 1 and 1 req', async () => {
-      const networkRecords = [
-        {url: 'http://example.com'},
-        {url: 'https://securepubads.g.doubleclick.net/gpt/js/pubads.js'},
-        {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
-      ];
-      sandbox.stub(chromeDriver, 'harFromMessages')
-        .returns(newHar(networkRecords));
-
-      const parsedUrls = networkRecords.map((request) => new URL(request.url));
-      const results = HasAds.audit(
-        {Network: {har: newHar(networkRecords), parsedUrls}});
-
-      expect(results).with.property('score', 1);
-      expect(results).with.property('details').property('numRequests').equal(1);
-    });
-
-    it('should have a score of 1 and 1 req alone', async () => {
-      const networkRecords = [
-        {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
-      ];
-      sandbox.stub(chromeDriver, 'harFromMessages')
-        .returns(newHar(networkRecords));
-
-      const parsedUrls = networkRecords.map((request) => new URL(request.url));
-      const results = HasAds.audit(
-        {Network: {har: newHar(networkRecords), parsedUrls}});
-
-      expect(results).with.property('score', 1);
-      expect(results).with.property('details').property('numRequests').equal(1);
-    });
-
-    it('should have a score of 1 and 3 reqs', async () => {
-      const networkRecords = [
-        {url: 'http://example.com'},
-        {url: 'https://securepubads.g.doubleclick.net/gpt/js/pubads.js'},
-        {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
-        {url: 'https://securepubads.g.doubleclick.net/other'},
-        {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
-        {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
-      ];
-      sandbox.stub(chromeDriver, 'harFromMessages')
-        .returns(newHar(networkRecords));
-
-      const parsedUrls = networkRecords.map((request) => new URL(request.url));
-      const results = HasAds.audit(
-        {Network: {har: newHar(networkRecords), parsedUrls}});
-
-      expect(results).with.property('score', 1);
-      expect(results).with.property('details').property('numRequests').equal(3);
-    });
+        expect(results).with.property('score', expectedScore);
+        expect(results).with.property('details').property('numRequests')
+          .equal(expectedNumRequests);
+      });
+    }
   });
 
   describe('numImpressions', () => {
-    it('should have a score of 0 for empty logs, 0 impressions', async () => {
-      const networkRecords = [];
-      sandbox.stub(chromeDriver, 'harFromMessages')
-        .returns(newHar(networkRecords));
+    const testCases = [
+      {
+        description: 'empty logs',
+        networkRecords: [],
+        expectedScore: 0,
+        expectedImpressions: 0,
+      },
+      {
+        description: 'non-empty logs',
+        networkRecords: [
+          {url: 'http://example.com'},
+          {url: 'https://securepubads.g.doubleclick.net/other'},
+        ],
+        expectedScore: 0,
+        expectedImpressions: 0,
+      },
+      {
+        description: 'logs with single impression',
+        networkRecords: [
+          {url: 'http://example.com'},
+          {url: 'https://securepubads.g.doubleclick.net/gpt/js/pubads.js'},
+          {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
+          {url: 'https://googlesyndication.com/pcs/view?bar=baz'},
+        ],
+        expectedScore: 1,
+        expectedImpressions: 1,
+      },
+      {
+        description: 'logs with multiple impressions',
+        networkRecords: [
+          {url: 'http://example.com'},
+          {url: 'https://securepubads.g.doubleclick.net/gpt/js/pubads.js'},
+          {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
+          {url: 'https://googlesyndication.com/pcs/view?bar=baz'},
+          {url: 'https://googlesyndication.com/pagead/adview?bar=baz'},
+        ],
+        expectedScore: 1,
+        expectedImpressions: 2,
+      },
+    ];
+    for (const {description, networkRecords,
+      expectedScore, expectedImpressions} of testCases) {
+      it(`should have score of ${expectedScore} for ${description} with` +
+        ` ${expectedImpressions} impressions`, () => {
+        sandbox.stub(chromeDriver, 'harFromMessages')
+          .returns(newHar(networkRecords));
 
-      const parsedUrls = networkRecords.map((request) => new URL(request.url));
-      const results = HasAds.audit(
-        {Network: {har: newHar(networkRecords), parsedUrls}});
+        const parsedUrls = networkRecords.map((request) =>
+          new URL(request.url));
+        const results = HasAds.audit(
+          {Network: {har: newHar(networkRecords), parsedUrls}});
 
-      expect(results).with.property('score', 0);
-      expect(results).with.property('details').
-        property('numImpressions').equal(0);
-    });
-
-    it('should have a score of 0, 0 impressions w/ nonempty logs', async () => {
-      const networkRecords = [
-        {url: 'http://example.com'},
-        {url: 'https://securepubads.g.doubleclick.net/other'},
-      ];
-      sandbox.stub(chromeDriver, 'harFromMessages')
-        .returns(newHar(networkRecords));
-
-      const parsedUrls = networkRecords.map((request) => new URL(request.url));
-      const results = HasAds.audit(
-        {Network: {har: newHar(networkRecords), parsedUrls}});
-
-      expect(results).with.property('score', 0);
-      expect(results).with.property('details').
-        property('numImpressions').equal(0);
-    });
-
-    it('should have a score of 1, 1 impression', async () => {
-      const networkRecords = [
-        {url: 'http://example.com'},
-        {url: 'https://securepubads.g.doubleclick.net/gpt/js/pubads.js'},
-        {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
-        {url: 'https://googlesyndication.com/pcs/view?bar=baz'},
-      ];
-      sandbox.stub(chromeDriver, 'harFromMessages')
-        .returns(newHar(networkRecords));
-
-      const parsedUrls = networkRecords.map((request) => new URL(request.url));
-      const results = HasAds.audit(
-        {Network: {har: newHar(networkRecords), parsedUrls}});
-
-      expect(results).with.property('score', 1);
-      expect(results).with.property('details').
-        property('numImpressions').equal(1);
-    });
-
-    it('should have a score of 1, 2 impressions', async () => {
-      const networkRecords = [
-        {url: 'http://example.com'},
-        {url: 'https://securepubads.g.doubleclick.net/gpt/js/pubads.js'},
-        {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo'},
-        {url: 'https://googlesyndication.com/pcs/view?bar=baz'},
-        {url: 'https://googlesyndication.com/pagead/adview?bar=baz'},
-      ];
-      sandbox.stub(chromeDriver, 'harFromMessages')
-        .returns(newHar(networkRecords));
-
-      const parsedUrls = networkRecords.map((request) => new URL(request.url));
-      const results = HasAds.audit(
-        {Network: {har: newHar(networkRecords), parsedUrls}});
-
-      expect(results).with.property('score', 1);
-      expect(results).with.property('details').
-        property('numImpressions').equal(2);
-    });
+        expect(results).with.property('score', expectedScore);
+        expect(results).with.property('details').property('numImpressions')
+          .equal(expectedImpressions);
+      });
+    }
   });
 });
--- test/utils/resource-classification_test.js
+++ test/utils/resource-classification_test.js
@@ -4,40 +4,49 @@ const {URL} = require('url');
 
 describe('resource-classification', () => {
   describe('#isGoogleAds', () => {
-    it('should return true for DoubleClick links w/o subdomains', () => {
-      const url = new URL('https://doubleclick.net/gpt/js/pubads.js');
-      expect(isGoogleAds(url)).to.be.true;
-    });
-
-    it('should return true for G-Syndication links w/o subdomains', () => {
-      const url = new URL('https://googlesyndication.com/gpt/js/pubads.js');
-      expect(isGoogleAds(url)).to.be.true;
-    });
-
-    it('should return true for DoubleClick links w/ hash+query', () => {
-      const url = new URL('https://www.doubleclick.net/tag/js/gpt.js?foo=bar#baz');
-      expect(isGoogleAds(url)).to.be.true;
-    });
-
-    it('should return true for G-Syndication links w/ hash+query', () => {
-      const url = new URL('https://www.googlesyndication.com/tag/js/gpt.js?foo=bar#baz');
-      expect(isGoogleAds(url)).to.be.true;
-    });
-
-    it('should return true for DoubleClick w/ subdomain links', () => {
-      const url = new URL('https://pagead2.doubleclick.net/pcs/activeview');
-      expect(isGoogleAds(url)).to.be.true;
-    });
-
-    it('should return true for G-Syndication w/ subdomain links', () => {
-      const url = new URL('https://pagead2.googlesyndication.com/pcs/activeview');
-      expect(isGoogleAds(url)).to.be.true;
-    });
-
-    it('should return false for any other URL', () => {
-      const url = new URL('https://facebook.com/foo?bar=baz#bat');
-      expect(isGoogleAds(url)).to.be.false;
-    });
+    const testCases = [
+      {
+        description: 'DoubleClick links without subdomains',
+        url: new URL('https://doubleclick.net/gpt/js/pubads.js'),
+        expectation: true,
+      },
+      {
+        description: 'Googlesyndication links without subdomains',
+        url: new URL('https://googlesyndication.com/gpt/js/pubads.js'),
+        expectation: true,
+      },
+      {
+        description: 'DoubleClick lnks with hash and query',
+        url: new URL('https://www.doubleclick.net/tag/js/gpt.js?foo=bar#baz'),
+        expectation: true,
+      },
+      {
+        description: 'Googlesyndication links with hash and query',
+        url: new URL('https://www.googlesyndication.com/tag/js/gpt.js?foo=bar#baz'),
+        expectation: true,
+      },
+      {
+        description: 'DoubleClick links with subdomains',
+        url: new URL('https://pagead2.doubleclick.net/pcs/activeview'),
+        expectation: true,
+      },
+      {
+        description: 'Googlesyndication links with subdomains',
+        url: new URL('https://pagead2.googlesyndication.com/pcs/activeview'),
+        expectation: true,
+      },
+      {
+        description: 'any other URL',
+        url: new URL('https://facebook.com/foo?bar=baz#bat'),
+        expectation: false,
+      },
+    ];
+    for (const {description, url, expectation} of testCases) {
+      it(`should return ${expectation} for ${description}`, () => {
+        const results = isGoogleAds(url);
+        expect(results).to.equal(expectation);
+      });
+    }
   });
 
   describe('#hasAdRequestPath', () => {
@@ -70,30 +79,39 @@ describe('resource-classification', () => {
   });
 
   describe('#isGpt', () => {
-    it('should return true for URLs that load gpt.js', () => {
-      const url = new URL('http://www.googletagservices.com/tag/js/gpt.js');
-      expect(isGpt(url)).to.be.true;
-    });
-
-    it('should return true for gpt.js loaded with hash', () => {
-      const url = new URL('https://www.googletagservices.com/tag/js/gpt.js#foo');
-      expect(isGpt(url)).to.be.true;
-    });
-
-    it('should return true for gpt.js loaded with query string', () => {
-      const url = new URL('https://www.googletagservices.com/tag/js/gpt.js?foo=bar');
-      expect(isGpt(url)).to.be.true;
-    });
-
-    it('should return true for gpt.js loaded with query string + hash', () => {
-      const url = new URL('https://www.googletagservices.com/tag/js/gpt.js?foo=bar#baz');
-      expect(isGpt(url)).to.be.true;
-    });
-
-    it('should return false for URLs that don\'t load gpt.js', () => {
-      const url = new URL('https://facebook.com/foo?bar=baz');
-      expect(isGpt(url)).to.be.false;
-    });
+    const testCases = [
+      {
+        description: 'URLs that load gpt.js',
+        url: new URL('http://www.googletagservices.com/tag/js/gpt.js'),
+        expectation: true,
+      },
+      {
+        description: 'URLs that load gpt.js with hash',
+        url: new URL('https://www.googletagservices.com/tag/js/gpt.js#foo'),
+        expectation: true,
+      },
+      {
+        description: 'URLs that load gpt.js with query string',
+        url: new URL('https://www.googletagservices.com/tag/js/gpt.js?foo=bar'),
+        expectation: true,
+      },
+      {
+        description: 'URLs that load gpt.js with query string and hash',
+        url: new URL('https://www.googletagservices.com/tag/js/gpt.js?foo=bar#baz'),
+        expectation: true,
+      },
+      {
+        description: 'URLs that don\'t load gpt.js',
+        url: new URL('https://facebook.com/foo?bar=baz'),
+        expectation: false,
+      },
+    ];
+    for (const {description, url, expectation} of testCases) {
+      it(`should return ${expectation} for ${description}`, () => {
+        const results = isGpt(url);
+        expect(results).to.equal(expectation);
+      });
+    }
   });
 
   describe('#isHttp', () => {
