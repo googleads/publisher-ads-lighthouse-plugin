8e24ee9063c142b45f8356447e742e16daca62e5
Benjamin Catarevas <bencatarevas@google.com>
2018-07-19 15:33:18 -0400

    Add dependency tree structure to output
    Change-Id: If0bbcfaf8138c0d671e5ea26c4f6bf43bb101efa

--- audits/ad-request-critical-path.js
+++ audits/ad-request-critical-path.js
@@ -4,47 +4,70 @@ const {URL} = require('url');
 
 /**
  * Finds the critical path and the number of items blocking gpt by using
- * iterative depth-first search.
+ * iterative depth-first search. Generates a set of resources blocking gpt, as
+ * well as a deconstructed tree to show the relationship between resources
+ * (where X is a child of Y if X blocks Y). A node will appear multiple
+ * times in the tree if it has multiple parents (i.e. if the underlying
+ * dependency graph is not a tree).
+ * To illustrate with an example, if Node A and Node B both depend on Node C,
+ * Node A would point to an instance of Node C, and Node B would point to an
+ * instance of Node C, rather than A and B pointing to one copy of C.
  * @param {HAR.Entry} startingEntry
  * @param {HAR.Har} har
- * @return {Set<string>}
+ * @return {{blockedRequests: Set<string>, treeRootNode: RequestTree.TreeNode}}
  */
 function findCriticalPath(startingEntry, har) {
   // TODO(bencatarevas): Find a data structure that can contain the DAG for the
   // stack. Also, determine a way to find the height of the DAG.
 
   /** @type {Set<string>} */ const blockedRequests = new Set();
-  /** @type {Array<string>} */ const stack = [startingEntry.request.url];
+  const treeRootNode = createNode(startingEntry.request.url);
+
+  /** @type {Array<{name: string, children: Array<RequestTree.TreeNode>}>} */
+  const stack = [treeRootNode];
   /** @type {Map<string, HAR.Entry>} */ const harMap = new Map();
+
+  /** @type {Set<string>} */
+  // Container to hold all visited edges of the dependency tree. This is to
+  // avoid either ignoring a repeated value that we want to add, or adding an
+  // unwanted repeated value to the tree.
+  const visitedEdges = new Set();
 
   for (const entry of har.log.entries) {
     harMap.set(entry.request.url, entry);
   }
 
   while (stack.length) {
-    const currentUrl = /** @type {string} */ (stack.pop());
-    blockedRequests.add(currentUrl);
+    const parentNode = /** @type {RequestTree.TreeNode} */ (stack.pop());
+    blockedRequests.add(parentNode.name);
 
     // Something to note here is that the check below serves two purposes.
     // The first is for checking if the URLs match. The second is for checking
     // whether we've reached the root domain. We don't actually add the root
-    // domain to harMap.Thus, we need to check if, when accessing the
+    // domain to harMap. Thus, we need to check if, when accessing the
     // harMap, it returns undefined to see if we've reached the root domain.
 
-    const currentEntry = harMap.get(currentUrl);
+    const currentEntry = harMap.get(parentNode.name);
     if (!currentEntry) {
       continue;
     }
     for (const callFrame of getCallFrames(currentEntry)) {
-      const hasVisited = blockedRequests.has(callFrame.url);
-      if (!hasVisited) {
+      // Concatenation of urls to store in set. We cannot store objects because
+      // Sets check reference equality for objects, not value equality.
+      const relationship = parentNode.name + callFrame.url;
+      const hasVisitedEdge = visitedEdges.has(relationship);
+
+      if (!hasVisitedEdge) {
+        const newChildNode = createNode(callFrame.url);
+        parentNode.children.push(newChildNode);
+        visitedEdges.add(relationship);
         blockedRequests.add(callFrame.url);
-        stack.push(callFrame.url);
+        stack.push(newChildNode);
       }
     }
   }
 
-  return blockedRequests;
+  return {blockedRequests, treeRootNode};
 }
 
 /**
@@ -75,6 +98,18 @@ function getInitiatorDetails(entry) {
     };
   }
   return /** @type {HAR.Initiator} */ (JSON.parse(entry._initiator_detail));
+}
+
+/**
+ * Generates a TreeNode object.
+ * @param {string} url
+ * @return {RequestTree.TreeNode}
+ */
+function createNode(url) {
+  return {
+    name: url,
+    children: [],
+  };
 }
 
 /**
@@ -111,17 +146,17 @@ class AdRequestCriticalPath extends Audit {
 
     // We assume that the first entry in adsEntries will be the first ad
     // request. When testing, the numbers match using adsEntries[0].
-    const blockedRequests = adsEntries.length ?
-      findCriticalPath(adsEntries[0], har) : new Set();
+    const {blockedRequests, treeRootNode} = adsEntries.length ?
+      findCriticalPath(adsEntries[0], har)
+      : {blockedRequests: new Set(), treeRootNode: {}};
     const numBlocked = blockedRequests.size;
-
     return {
       rawValue: numBlocked,
       score: numBlocked > 2 ? 0 : 1,
       displayValue: `${numBlocked} resource(s) block ads`,
       details: {
         numBlocked,
-        blockedRequests,
+        treeRootNode,
       },
     };
   }
