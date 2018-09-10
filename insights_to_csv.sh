bb7cd3f46f0ba9f8a47bdb0c96fac7824b57ad8a
Jon Burger <jburger@google.com>
2018-09-10 15:26:28 -0400

    Modified script to produce a csv given a folder of audits Default mode is to use existing json but `-r` flag will rerun the audits with the existing aritifacts
    Change-Id: I350d53509a93bce7e298893a00bd7cf66e887b60

--- insights_to_csv.sh
+++ insights_to_csv.sh
@@ -1,33 +1,81 @@
 #!/bin/bash
-# Usage: `./insights_to_csv.sh {URL} {File to add or append to}`
-if [ "$#" -ne 2 ]; then
-    echo "2 params needed, url and filename."
+# Usage: `./insights_to_csv.sh output-path.csv -r`
+# Optional param `-r`-runs audits, otherwise will use existing reports
+if [[ "$#" -ne 1 && "$#" -ne 2 ]]
+then
+    echo "At least 1 param needed, output-path."
     exit 1;
 fi
-fields=(SCORE RAW_VAL);
-vals=( $(node index.js $1 --quiet --output json |
-  jq -r '.audits[] | "\(.id)\n\(.score)\n\(.rawValue)"' ) );
 
-headString='url';
-valString=$1;
-valOffset=$((${#fields[@]}+1));
-vIndex=0;
-for val in "${vals[@]}";
-do
-  if [ $((vIndex%valOffset)) -eq 0 ];
- then
-   for field in "${fields[@]}";
-   do
-     headString="$headString,$val $field";
-   done;
- else
-   valString=$valString,$val;
- fi;
- vIndex=$(($vIndex+1));
-done;
+runsPath=~/'DriveFileStream/Team Drives/AdSpeed/Insights   Lampad/runs';
 
-if [ ! -f $2 ]; then
-    echo $headString > $2;
-fi
-echo $valString >> $2;
+tempOutPath='temp-out-'$(date +%F_%H-%M-%S)'.json';
+echo '{ "outputs": [' > $tempOutPath
+find "$runsPath" -mindepth 1 -maxdepth 1 -type d |head -n 50| while read line; do
+  if [[ $2 == -r ]]; then
+    $(node index.js -A "$line" --output json --quiet >> $tempOutPath) &&\
+      $(echo -n , >> $tempOutPath)
+  else
+    reportPath=$(ls "$line"/*.report.json | head -1);
+    $(cat "$reportPath" >> $tempOutPath) && $(echo -n , >> $tempOutPath)
+  fi;
+done
+truncate -s-1 $tempOutPath
+echo ]} >> $tempOutPath
+
+headString='
+  url,
+  ad-blocking-tasks_score,
+  ad-blocking-tasks_raw,
+  ad-request-critical-path_score,
+  ad-request-critical-path_raw,
+  ad-request-from-page-start_score,
+  ad-request-from-page-start_raw,
+  ad-request-from-tag-load_score,
+  ad-request-from-tag-load_raw,
+  ads-in-viewport_score,
+  ads-in-viewport_raw,
+  async-ad-tags_score,
+  async-ad-tags_raw,
+  loads-gpt-over-https_score,
+  loads-gpt-over-https_raw,
+  serial-header-bidding_score,
+  serial-header-bidding_raw,
+  static-ad-tags_score,
+  static-ad-tags_raw,
+  tag-load-time_score,
+  tag-load-time_raw,
+  viewport-ad-density_score,
+  viewport-ad-density_raw'
+
+jqScript='
+  .outputs[] as $o | $o.audits |
+  .["ad-blocking-tasks"] as $abt |
+  .["ad-request-critical-path"] as $arcp |
+  .["ad-request-from-page-start"] as $arps |
+  .["ad-request-from-tag-load"] as $artl |
+  .["ads-in-viewport"] as $avp |
+  .["async-ad-tags"] as $aat |
+  .["loads-gpt-over-https"] as $lgoh |
+  .["serial-header-bidding"] as $shb |
+  .["static-ad-tags"] as $sat |
+  .["tag-load-time"] as $tlt |
+  .["viewport-ad-density"] as $vad |
+  [$o.finalUrl,
+  $abt.score, $abt.rawValue,
+  $arcp.score, $arcp.rawValue,
+  $arps.score, $arps.rawValue,
+  $artl.score, $artl.rawValue,
+  $avp.score, $avp.rawValue,
+  $aat.score, $aat.rawValue,
+  $lgoh.score, $lgoh.rawValue,
+  $shb.score, $shb.rawValue,
+  $sat.score, $sat.rawValue,
+  $tlt.score, $tlt.rawValue,
+  $vad.score, $vad.rawValue] |
+  @csv'
+
+echo $headString > $1
+eval "jq -r '$jqScript' $tempOutPath" >> $1
+rm $tempOutPath
 
