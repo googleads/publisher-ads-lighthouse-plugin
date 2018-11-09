2832e04d007cc99c43dfa35a1506d3f89a1d9975
Jon Burger <jburger@google.com>
2018-11-09 14:08:46 -0500

    Add license header to all files.
    Add LICENSE file.
    
    Add CONTRIBUTING.md file.
    
    Change-Id: Id26e9bfb417182cc64d27021696dadfe13d2c6b4

--- audits/ad-blocking-tasks.js
+++ audits/ad-blocking-tasks.js
@@ -1,9 +1,23 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 const {auditNotApplicable} = require('../utils/builder');
 const {Audit} = require('lighthouse');
 const {isGoogleAds} = require('../utils/resource-classification');
 const {URL} = require('url');
 /**
- * Threshold for long task duration (ms), from http://go/w3c-github/longtasks.
+ * Threshold for long task duration (ms), from https://github.com/w3c/longtasks.
  */
 const LONG_TASK_DUR_MS = 50;
 
