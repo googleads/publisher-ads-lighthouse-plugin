44ba53e17833df3593daa54bc4a350d6011ba31c
Jon Burger <jburger@google.com>
2018-11-09 14:08:46 -0500

    Add license header to all files.
    Add LICENSE file.
    
    Add CONTRIBUTING.md file.
    
    Change-Id: Id26e9bfb417182cc64d27021696dadfe13d2c6b4

--- typings/har.d.ts
+++ typings/har.d.ts
@@ -1,3 +1,17 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Contains typings for HAR json objects. The HAR format spec can be found at
 // https://dvcs.w3.org/hg/webperf/raw-file/tip/specs/HAR/Overview.html.
 // Note that the spec allows for custom fields to be added. These fields must be
