fa5364414311e8fa0febc6934c3ba1815c90f3ed
Benjamin Catarevas <bencatarevas@google.com>
2018-11-13 17:54:06 -0500

    Add Serial Header Bidding audit.
    Change-Id: Icc624cadfac5bba0d56e80bcc03b8972e2b47ccc

---
+++ audits/serial-header-bidding.js
@@ -0,0 +1,206 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const NetworkRecords = require('lighthouse/lighthouse-core/gather/computed/network-records');
+const {Audit} = require('lighthouse');
+const {bucket} = require('../utils/array');
+const {getPageStartTime} = require('../utils/network-timing');
+const {isGoogleAds, getHeaderBidder} = require('../utils/resource-classification');
+const {URL} = require('url');
+
+// Min record duration (s) to be considered a bid.
+const MIN_BID_DURATION = .05;
+
+/**
+ * @typedef {Object} BidRequest
+ * @property {string | boolean} bidder
+ * @property {string} url
+ * @property {number} startTime
+ * @property {number} endTime
+ * @property {number} duration
+ */
+
+/**
+ * Table headings for audits details sections.
+ * @type {LH.Audit.Heading[]}
+ */
+const HEADINGS = [
+  {key: 'bidder', itemType: 'text', text: 'Bidder'},
+  {key: 'url', itemType: 'url', text: 'URL'},
+  {key: 'startTime', itemType: 'ms', text: 'Start Time'},
+  {key: 'endTime', itemType: 'ms', text: 'End Time'},
+  {key: 'duration', itemType: 'ms', text: 'Duration'},
+];
+
+/**
+ * Enum for type of request.
+ * @readonly
+ * @enum {string}
+ */
+const RequestType = {
+  AD: 'ad',
+  BID: 'bid',
+  UNKNOWN: 'unknown',
+};
+
+/**
+ * Makes shallow copy of records.
+ * @param {Array<LH.Artifacts.NetworkRequest>} records
+ * @param {string} recordType
+ * @param {number} originTime
+ * @return {Array<NetworkDetails.RequestRecord>}
+ */
+function constructRecords(records, recordType, originTime) {
+  return records.map((record) => ({
+    // We offset the start time so that the origin can be relative.
+    // Previously, it was at an extremely high value.
+    startTime: record.startTime - originTime,
+    endTime: record.endTime - originTime,
+    url: record.url,
+    type: recordType,
+  }));
+}
+
+/**
+ * Checks the type of record.
+ * @param {LH.Artifacts.NetworkRequest} record
+ * @return {string}
+ */
+function checkRecordType(record) {
+  if (isGoogleAds(new URL(record.url))) {
+    return RequestType.AD;
+  } else if (getHeaderBidder(record.url)) {
+    return RequestType.BID;
+  } else {
+    return RequestType.UNKNOWN;
+  }
+}
+
+/**
+ * Filter out requests without responses, image responses, and responses
+ * taking less than 50ms.
+ * @param {LH.Artifacts.NetworkRequest} rec
+ * @return {boolean}
+ */
+function isValidRecord(rec) {
+  return (rec.resourceSize == null || rec.resourceSize > 0) &&
+      (rec.resourceType != 'Image') &&
+      (rec.endTime - rec.startTime >= MIN_BID_DURATION);
+}
+
+/**
+ * Audit to check if serial header bidding occurs
+ */
+class SerialHeaderBidding extends Audit {
+  /**
+   * @return {LH.Audit.Meta}
+   * @override
+   */
+  static get meta() {
+    return {
+      id: 'serial-header-bidding',
+      title: 'No serial header bidding detected',
+      failureTitle: 'There are serial header bidding requests',
+      description: 'Running header bidding serially make ads load slower, ' +
+          'which has an impact on impressions. Consider running in parallel.',
+      requiredArtifacts: ['devtoolsLogs'],
+    };
+  }
+
+  /**
+   * @param {LH.Artifacts} artifacts
+   * @param {LH.Audit.Context} context
+   * @return {Promise<LH.Audit.Product>}
+   */
+  static async audit(artifacts, context) {
+    const devtoolsLogs = artifacts.devtoolsLogs[Audit.DEFAULT_PASS];
+    const unfilteredNetworkRecords =
+    await NetworkRecords.request(devtoolsLogs, context);
+
+    // Filter out requests without responses, image responses, and responses
+    // taking less than 50ms.
+    const networkRecords = unfilteredNetworkRecords
+        .filter(isValidRecord);
+
+    // We filter for URLs that are related to header bidding.
+    // Then we create shallow copies of each record. This is because the records
+    // by default have circular structure, which causes an error to be thrown
+    // when we return them unmodified in the details field of the audit.
+    // So, we create objects that only have the relevant information, and return
+    // in the details field.
+    const recordsByType = bucket(networkRecords, checkRecordType);
+
+    // networkRecords are sorted by start time. We use this to offset
+    // the visualized records.
+    const timeOffset = getPageStartTime(unfilteredNetworkRecords, 0);
+
+    // Construct shallow copies of records. If no records are found, return [].
+    const adsRecords = constructRecords(
+      recordsByType[RequestType.AD] || [], RequestType.AD, timeOffset);
+    const headerBiddingRecords = constructRecords(
+      recordsByType[RequestType.BID] || [], RequestType.BID, timeOffset);
+    /** @type {Object<string, BidRequest>} */
+    const bidRequests = {};
+
+    let hasSerialHeaderBidding;
+    const validHeaderBiddingRecords = [];
+    if (headerBiddingRecords.length <= 1) {
+      hasSerialHeaderBidding = false;
+    } else {
+      let previousHost = '';
+      for (const record of headerBiddingRecords) {
+        const startTime = record.startTime * 1000;
+        const endTime = record.endTime * 1000;
+        const duration = endTime - startTime;
+        const url = new URL(record.url);
+        const host = url.host;
+        const path = url.pathname;
+
+        const isSerialRequest = !previousHost.length ||
+          startTime >= bidRequests[previousHost].endTime;
+
+        if (!bidRequests[host] && isSerialRequest) {
+          bidRequests[host] = {
+            bidder: getHeaderBidder(record.url),
+            url: host + path,
+            startTime,
+            endTime,
+            duration,
+          };
+          previousHost = host;
+          validHeaderBiddingRecords.push(record);
+        }
+      }
+
+      // This means that at least 2 requests to different hosts occurred
+      // serially.
+      hasSerialHeaderBidding = Object.keys(bidRequests).length > 1;
+    }
+
+    return {
+      rawValue: hasSerialHeaderBidding,
+      score: hasSerialHeaderBidding ? 0 : 1,
+      displayValue: hasSerialHeaderBidding ? `Has serial header bidding` : '',
+      details: SerialHeaderBidding.makeTableDetails(
+        HEADINGS, Object.values(bidRequests)),
+      extendedInfo: {
+        adsRecords,
+        headerBiddingRecords:
+          hasSerialHeaderBidding ? validHeaderBiddingRecords : [],
+      },
+    };
+  }
+}
+
+module.exports = SerialHeaderBidding;
---
+++ test/audits/serial-header-bidding_test.js
@@ -0,0 +1,397 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const NetworkRecords = require('lighthouse/lighthouse-core/gather/computed/network-records');
+const SerialHeaderBidding = require('../../audits/serial-header-bidding');
+const sinon = require('sinon');
+const {expect} = require('chai');
+
+describe('SerialHeaderBidding', async () => {
+  let sandbox;
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+  });
+  afterEach(() => {
+    sandbox.restore();
+  });
+  describe('hasSerialHeaderBidding', async () => {
+    const testCases = [
+      {
+        desc: 'should pass for empty records',
+        networkRecords: [],
+        expectedScore: 1,
+      },
+      {
+        desc: 'should pass for records with non-header bidding domains',
+        networkRecords: [
+          {url: 'https://example.com', startTime: 5, endTime: 10, statusCode: 200},
+          {url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo', startTime: 10, endTime: 15},
+          {url: 'https://www.googletagservices.com/tag/js/gpt.js', startTime: 15, endTime: 20},
+        ],
+        expectedScore: 1,
+      },
+      {
+        desc: 'should fail for records with header bidding domains',
+        networkRecords: [
+          {url: 'https://aax.amazon-adsystem.com/e/dtb/bid', startTime: 5, endTime: 10},
+          {url: 'http://contextual.media.net/bidexchange.js', startTime: 10, endTime: 15},
+        ],
+        expectedScore: 0,
+      },
+      {
+        desc: 'should fail for records with max start time = min end time',
+        networkRecords: [
+          {url: 'https://aax.amazon-adsystem.com/e/dtb/bid', startTime: 5, endTime: 10},
+          {url: 'http://contextual.media.net/bidexchange.js', startTime: 10, endTime: 15},
+        ],
+        expectedScore: 0,
+      },
+      {
+        desc: 'should fail for records with max start time > min end time',
+        networkRecords: [
+          {url: 'https://aax.amazon-adsystem.com/e/dtb/bid', startTime: 5, endTime: 10},
+          {url: 'http://contextual.media.net/bidexchange.js', startTime: 15, endTime: 20},
+        ],
+        expectedScore: 0,
+      },
+      {
+        desc: 'should pass for records with max start time < min end time',
+        networkRecords: [
+          {url: 'https://aax.amazon-adsystem.com/e/dtb/bid', startTime: 1, endTime: 3},
+          {url: 'http://contextual.media.net/bidexchange.js', startTime: 2, endTime: 15},
+        ],
+        expectedScore: 1,
+      },
+      {
+        desc: 'should fail for multiple records',
+        networkRecords: [
+          {url: 'https://aax.amazon-adsystem.com/e/dtb/bid', startTime: 5, endTime: 10},
+          {url: 'http://contextual.media.net/bidexchange.js', startTime: 7, endTime: 9},
+          {url: 'http://as.casalemedia.com/cygnus', startTime: 15, endTime: 20},
+        ],
+        expectedScore: 0,
+      },
+      {
+        desc: 'should ignore resources with 0 resource size',
+        networkRecords: [
+          {url: 'https://aax.amazon-adsystem.com/e/dtb/bid', startTime: 5, endTime: 10, resourceSize: 100},
+          {url: 'http://contextual.media.net/bidexchange.js', startTime: 7, endTime: 9, resourceSize: 200},
+          {url: 'http://as.casalemedia.com/cygnus', startTime: 15, endTime: 20, resourceSize: 0},
+        ],
+        // Passes since the final (serial) request has no response body.
+        expectedScore: 1,
+      },
+      {
+        desc: 'should pass for one record',
+        networkRecords: [
+          {url: 'http://as.casalemedia.com/cygnus', startTime: 5, endTime: 10},
+        ],
+        expectedScore: 1,
+      },
+    ];
+    for (const {desc, networkRecords, expectedScore} of testCases) {
+      it(`${desc} with score of ${expectedScore}`, async () => {
+        sandbox.stub(NetworkRecords, 'request').returns(networkRecords);
+        const results = await SerialHeaderBidding.audit({devtoolsLogs: {}}, {});
+        expect(results).to.have.property('score', expectedScore);
+      });
+    }
+  });
+
+  describe('checkFilteredRecords', async () => {
+    const testCases = [
+      {
+        desc: '0 ads and bidding records for empty network records',
+        networkRecords: [],
+        expectedAdsRecords: [],
+        expectedHeaderBiddingRecords: [],
+      },
+      {
+        desc: '0 ads and 0 bidding records for non-ads/non-bidding records',
+        networkRecords: [
+          {
+            startTime: 10,
+            endTime: 20,
+            url: 'https://example.com',
+            statusCode: 200,
+          },
+          {
+            startTime: 10,
+            endTime: 20,
+            url: 'https://foo.com',
+          },
+        ],
+        expectedAdsRecords: [],
+        expectedHeaderBiddingRecords: [],
+      },
+      {
+        desc: '1 ads and 0 bidding records',
+        networkRecords: [
+          {
+            startTime: 0,
+            endTime: 10,
+            url: 'https://example.com',
+            statusCode: 200,
+          },
+          {
+            startTime: 10,
+            endTime: 20,
+            url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo',
+          },
+        ],
+        expectedAdsRecords: [
+          {
+            startTime: 10,
+            endTime: 20,
+            url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo',
+            type: 'ad',
+          },
+        ],
+        expectedHeaderBiddingRecords: [],
+      },
+      {
+        desc: '0 ads and 2 bidding records',
+        networkRecords: [
+          {
+            startTime: 10,
+            endTime: 20,
+            url: 'https://example.com',
+            statusCode: 200,
+          },
+          {
+            startTime: 25,
+            endTime: 35,
+            url: 'https://aax.amazon-adsystem.com/e/dtb/bid',
+            statusCode: 200,
+          },
+          {
+            startTime: 36,
+            endTime: 40,
+            url: 'https://bidder.criteo.com/cdb/',
+          },
+        ],
+        expectedAdsRecords: [],
+        expectedHeaderBiddingRecords: [
+          {
+            startTime: 15,
+            endTime: 25,
+            url: 'https://aax.amazon-adsystem.com/e/dtb/bid',
+            type: 'bid',
+          },
+          {
+            startTime: 26,
+            endTime: 30,
+            url: 'https://bidder.criteo.com/cdb/',
+            type: 'bid',
+          },
+        ],
+      },
+      {
+        desc: '1 ads and 2 bidding records',
+        networkRecords: [
+          {
+            startTime: 10,
+            endTime: 20,
+            url: 'https://example.com',
+            statusCode: 200,
+          },
+          {
+            startTime: 15,
+            endTime: 30,
+            url: 'https://foo.com',
+          },
+          {
+            startTime: 25,
+            endTime: 35,
+            url: 'https://bidder.criteo.com/cdb/',
+          },
+          {
+            startTime: 40,
+            endTime: 50,
+            url: 'https://aax.amazon-adsystem.com/e/dtb/bid',
+            statusCode: 200,
+          },
+          {
+            startTime: 30,
+            endTime: 40,
+            url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo',
+          },
+        ],
+        expectedAdsRecords: [
+          {
+            startTime: 20,
+            endTime: 30,
+            url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo',
+            type: 'ad',
+          },
+        ],
+        expectedHeaderBiddingRecords: [
+          {
+            startTime: 15,
+            endTime: 25,
+            url: 'https://bidder.criteo.com/cdb/',
+            type: 'bid',
+          },
+          {
+            startTime: 30,
+            endTime: 40,
+            url: 'https://aax.amazon-adsystem.com/e/dtb/bid',
+            type: 'bid',
+          },
+        ],
+      },
+      {
+        desc: '1 ads and 1 bidding records with 0 resource size',
+        networkRecords: [
+
+          {
+            startTime: 25,
+            endTime: 35,
+            url: 'https://bidder.criteo.com/cdb/',
+            resourceSize: 0,
+          },
+          {
+            startTime: 30,
+            endTime: 40,
+            url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo',
+            resourceSize: 0,
+          },
+        ],
+        expectedAdsRecords: [],
+        expectedHeaderBiddingRecords: [],
+      },
+      {
+        desc: '1 ads and 2 bidding records with positive resource size',
+        networkRecords: [
+          {
+            startTime: 0,
+            endTime: 20,
+            url: 'https://aax.amazon-adsystem.com/e/dtb/bid',
+            statusCode: 200,
+          },
+          {
+            startTime: 25,
+            endTime: 35,
+            url: 'https://bidder.criteo.com/cdb/',
+            resourceSize: 100,
+          },
+          {
+            startTime: 30,
+            endTime: 40,
+            url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo',
+            resourceSize: 100,
+          },
+        ],
+        expectedAdsRecords: [
+          {
+            startTime: 30,
+            endTime: 40,
+            url: 'https://securepubads.g.doubleclick.net/gampad/ads?foo',
+            type: 'ad',
+          },
+        ],
+        expectedHeaderBiddingRecords: [
+          {
+            startTime: 0,
+            endTime: 20,
+            url: 'https://aax.amazon-adsystem.com/e/dtb/bid',
+            type: 'bid',
+          },
+          {
+            startTime: 25,
+            endTime: 35,
+            url: 'https://bidder.criteo.com/cdb/',
+            type: 'bid',
+          },
+        ],
+      },
+      {
+        desc: 'multiple ads and multiple bidding records',
+        networkRecords: [
+          {
+            startTime: 10,
+            endTime: 20,
+            url: 'https://example.com',
+            statusCode: 200,
+          },
+          {
+            startTime: 15,
+            endTime: 30,
+            url: 'https://foo.uk.com',
+          },
+          {
+            startTime: 35,
+            endTime: 40,
+            url: 'https://aax.amazon-adsystem.com/e/dtb/bid',
+          },
+          {
+            startTime: 45,
+            endTime: 55,
+            url: 'http://contextual.media.net/bidexchange.js',
+          },
+          {
+            startTime: 65,
+            endTime: 70,
+            url: 'https://securepubads.g.doubleclick.net/gampad/ads?bar',
+          },
+          {
+            startTime: 75,
+            endTime: 80,
+            url: 'https://googlesyndication.com/gpt/',
+          },
+        ],
+        expectedAdsRecords: [
+          {
+            startTime: 55,
+            endTime: 60,
+            url: 'https://securepubads.g.doubleclick.net/gampad/ads?bar',
+            type: 'ad',
+          },
+          {
+            startTime: 65,
+            endTime: 70,
+            url: 'https://googlesyndication.com/gpt/',
+            type: 'ad',
+          },
+        ],
+        expectedHeaderBiddingRecords: [
+          {
+            startTime: 25,
+            endTime: 30,
+            url: 'https://aax.amazon-adsystem.com/e/dtb/bid',
+            type: 'bid',
+          },
+          {
+            startTime: 35,
+            endTime: 45,
+            url: 'http://contextual.media.net/bidexchange.js',
+            type: 'bid',
+          },
+        ],
+      },
+    ];
+
+    for (const {desc, networkRecords, expectedAdsRecords,
+      expectedHeaderBiddingRecords} of testCases) {
+      it(`should have ${desc}`, async () => {
+        sandbox.stub(NetworkRecords, 'request').returns(networkRecords);
+        const results = await SerialHeaderBidding.audit({devtoolsLogs: {}}, {});
+        expect(results).with.property('extendedInfo')
+            .property('adsRecords').eql(expectedAdsRecords);
+        expect(results).with.property('extendedInfo')
+            .property('headerBiddingRecords').eql(expectedHeaderBiddingRecords);
+      });
+    }
+  });
+});
