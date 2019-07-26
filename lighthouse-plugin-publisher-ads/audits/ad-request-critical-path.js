1cab06d83c984084f8fd8073bf9765d360c06fb6
Warren Godone Maresca <warrengm@google.com>
2019-07-26 13:51:13 -0400

    Move code around

--- lighthouse-plugin-publisher-ads/audits/ad-request-critical-path.js
+++ lighthouse-plugin-publisher-ads/audits/ad-request-critical-path.js
@@ -13,15 +13,12 @@
 // limitations under the License.
 
 const i18n = require('lighthouse/lighthouse-core/lib/i18n/i18n');
-const NetworkRecords = require('lighthouse/lighthouse-core/computed/network-records');
 const {auditNotApplicable} = require('../messages/common-strings');
 const {Audit} = require('lighthouse');
-const {getAbbreviatedUrl, trimUrl} = require('../utils/resource-classification');
-const {getAdCriticalGraph} = require('../utils/graph');
-const {getTimingsByRecord} = require('../utils/network-timing');
+const {computeAdRequestWaterfall} = require('../utils/graph');
 
-/** @typedef {LH.Artifacts.NetworkRequest} NetworkRequest */
 /** @typedef {LH.Gatherer.Simulation.NodeTiming} NodeTiming */
+/** @typedef {import('../utils/graph').SimpleRequest} SimpleRequest */
 
 const UIStrings = {
   title: 'Minimal requests found in ad critical path',
@@ -40,16 +37,6 @@ const UIStrings = {
 };
 
 const str_ = i18n.createMessageInstanceIdFn(__filename, UIStrings);
-
-/**
- * @typedef {Object} SimpleRequest
- * @property {string} url
- * @property {string} abbreviatedUrl
- * @property {string} type
- * @property {number} startTime
- * @property {number} endTime
- * @property {number} duration
- */
 
 /**
  * Table headings for audits details sections.
@@ -79,64 +66,6 @@ const HEADINGS = [
     granularity: 1,
   },
 ];
-
-/**
- * Checks if two requests are similar enough to be merged.
- * @param {SimpleRequest} r1
- * @param {SimpleRequest} r2
- * @return {boolean}
- */
-function areSimilarRequests(r1, r2) {
-  if (Math.max(r1.startTime, r2.startTime) > Math.min(r1.endTime, r2.endTime)) {
-    return false;
-  }
-  if (r1.type != r2.type) {
-    return false;
-  }
-  return r1.abbreviatedUrl == r2.abbreviatedUrl;
-}
-
-/**
- * Summarizes the given array of requests by merging overlapping requests with
- * the same url. The resulting array will be ordered by start time.
- * @param {SimpleRequest[]} requests
- * @return {SimpleRequest[]}
- */
-function computeSummaries(requests) {
-  // Sort requests by URL first since we will merge overlapping records with
-  // the same URL below, using a similar algorithm to std::unique.
-  // Within a url, we sort by time to make overlap checks easier.
-  requests.sort((a, b) => {
-    if (a.abbreviatedUrl != b.abbreviatedUrl) {
-      return a.abbreviatedUrl < b.abbreviatedUrl ? -1 : 1;
-    }
-    if (a.type != b.type) {
-      return a.type < b.type ? -1 : 1;
-    }
-    if (a.startTime != b.startTime) {
-      return a.startTime < b.startTime ? -1 : 1;
-    }
-    return a.endTime - b.endTime;
-  });
-  const result = [];
-  for (let i = 0; i < requests.length; i++) {
-    const current = requests[i];
-    let next;
-    while (i < requests.length) {
-      next = requests[i + 1];
-      if (!next || !areSimilarRequests(next, current)) {
-        break;
-      }
-      current.url = current.abbreviatedUrl;
-      current.endTime = Math.max(current.endTime, next.endTime);
-      current.duration = current.endTime - current.startTime;
-      i++;
-    }
-    result.push(current);
-  }
-  result.sort((a, b) => a.startTime - b.startTime);
-  return result;
-}
 
 /**
  * Comptues the depth of the loading graph by comparing timings.
@@ -176,66 +105,33 @@ class AdRequestCriticalPath extends Audit {
     };
   }
 
-  static async computeResults(artifacts, context) {
-    const trace = artifacts.traces[Audit.DEFAULT_PASS];
-    const devtoolsLog = artifacts.devtoolsLogs[Audit.DEFAULT_PASS];
-    const networkRecords = await NetworkRecords.request(devtoolsLog, context);
-    const criticalRequests = getAdCriticalGraph(
-      networkRecords, trace.traceEvents);
-
-    /** @type {Map<NetworkRequest, NodeTiming>} */
-    const timingsByRecord =
-        await getTimingsByRecord(trace, devtoolsLog, context);
-    const REQUEST_TYPES = new Set([
-      'Script', 'XHR', 'Fetch', 'EventStream', 'Document', undefined]);
-    const blockingRequests = Array.from(criticalRequests)
-        .filter((r) => REQUEST_TYPES.has(r.resourceType))
-        .filter((r) => r.mimeType != 'text/css');
-
-    if (!blockingRequests.length) {
-      return auditNotApplicable.NoAds;
-    }
-    const tableView = blockingRequests.map((req) => {
-      const {startTime, endTime} = timingsByRecord.get(req) || req;
-      return {
-        startTime,
-        endTime,
-        duration: endTime - startTime,
-        url: trimUrl(req.url),
-        abbreviatedUrl: getAbbreviatedUrl(req.url),
-        type: req.resourceType,
-      };
-    });
-    return computeSummaries(tableView)
-        .filter((r) => r.duration > 30 && r.startTime > 0);
-  }
-
   /**
    * @param {LH.Artifacts} artifacts
    * @param {LH.Audit.Context} context
    * @return {Promise<LH.Audit.Product>}
    */
   static async audit(artifacts, context) {
-    try {
-      const tableView =
-      await AdRequestCriticalPath.computeResults(artifacts, context);
+    const trace = artifacts.traces[Audit.DEFAULT_PASS];
+    const devtoolsLog = artifacts.devtoolsLogs[Audit.DEFAULT_PASS];
 
-      const depth = computeDepth(tableView);
-      const failed = depth > 3;
-
-      return {
-        numericValue: depth,
-        score: failed ? 0 : 1,
-        displayValue: str_(UIStrings.displayValue,
-          {
-            serialResources: depth,
-            totalResources: tableView.length,
-          }),
-        details: AdRequestCriticalPath.makeTableDetails(HEADINGS, tableView),
-      };
-    } catch (e) {
-      console.log(e);
+    const tableView =
+      await computeAdRequestWaterfall(trace, devtoolsLog, context);
+    if (!tableView.length) {
+      return auditNotApplicable.NoAds;
     }
+    const depth = computeDepth(tableView);
+    const failed = depth > 3;
+
+    return {
+      numericValue: depth,
+      score: failed ? 0 : 1,
+      displayValue: str_(UIStrings.displayValue,
+        {
+          serialResources: depth,
+          totalResources: tableView.length,
+        }),
+      details: AdRequestCriticalPath.makeTableDetails(HEADINGS, tableView),
+    };
   }
 }
 
--- lighthouse-plugin-publisher-ads/audits/critical-blocking-requests.js
+++ lighthouse-plugin-publisher-ads/audits/critical-blocking-requests.js
@@ -12,14 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const AdRequestCriticalPath = require('./ad-request-critical-path');
 const i18n = require('lighthouse/lighthouse-core/lib/i18n/i18n');
-const NetworkRecords = require('lighthouse/lighthouse-core/computed/network-records');
 const {auditNotApplicable} = require('../messages/common-strings');
 const {Audit} = require('lighthouse');
-const {getAbbreviatedUrl, trimUrl} = require('../utils/resource-classification');
-const {getAdCriticalGraph} = require('../utils/graph');
-const {getTimingsByRecord} = require('../utils/network-timing');
+const {computeAdRequestWaterfall} = require('../utils/graph');
 
 /** @typedef {LH.Artifacts.NetworkRequest} NetworkRequest */
 /** @typedef {LH.Gatherer.Simulation.NodeTiming} NodeTiming */
@@ -38,16 +34,6 @@ const UIStrings = {
 
 const str_ = i18n.createMessageInstanceIdFn(__filename, UIStrings);
 
-/**
- * @typedef {Object} SimpleRequest
- * @property {string} url
- * @property {string} abbreviatedUrl
- * @property {string} type
- * @property {number} startTime
- * @property {number} endTime
- * @property {number} duration
- * @property {?number?} selfTime
- */
 
 /**
  * Table headings for audits details sections.
@@ -73,43 +59,6 @@ const HEADINGS = [
   },
 ];
 
-/**
- * @param {SimpleRequest[]} requests A pre-sorted list of requests by start
- *   time.
- * @return {SimpleRequest[]}
- */
-function findBlockingRequests(requests) {
-  let bottlneckRequest = requests[0];
-  bottlneckRequest.selfTime = bottlneckRequest.duration;
-
-  let scanEnd = bottlneckRequest.startTime;
-
-  const results = [];
-  for (const current of requests) {
-    if (current.endTime < scanEnd || current == bottlneckRequest) {
-      // Overlaps with previous requests, skip to avoid double counting.
-      continue;
-    }
-    const left = Math.max(scanEnd, current.startTime);
-    const right = Math.min(bottlneckRequest.endTime, current.endTime);
-    if (left < right) {
-      // @ts-ignore selfTime is initialized elsewhere, so it won't be undefined.
-      bottlneckRequest.selfTime -= (right - left);
-    }
-    scanEnd = Math.max(scanEnd, right);
-    if (current.endTime > bottlneckRequest.endTime) {
-      // The next request is a potential bottleneck.
-      if (bottlneckRequest.selfTime > 100) {
-        bottlneckRequest.initiatorUrl = bottlneckRequest.initiatorRequest ?
-          bottlneckRequest.initiatorRequest.url : '',
-        results.push(bottlneckRequest);
-      }
-      current.selfTime = current.endTime - left;
-      bottlneckRequest = current;
-    }
-  }
-  return results;
-}
 
 /**
  * Audit to check the length of the critical path to load ads.
@@ -136,25 +85,30 @@ class CriticalBlockingRequests extends Audit {
    * @return {Promise<LH.Audit.Product>}
    */
   static async audit(artifacts, context) {
+    const trace = artifacts.traces[Audit.DEFAULT_PASS];
+    const devtoolsLog = artifacts.devtoolsLogs[Audit.DEFAULT_PASS];
+
     const waterfall =
-      await AdRequestCriticalPath.computeResults(artifacts, context);
+      await computeAdRequestWaterfall(trace, devtoolsLog, context);
     if (!waterfall.length) {
       return auditNotApplicable.NoAdRelatedReq;
     }
-    const criticalRequests = findBlockingRequests(waterfall);
-    // Only show the top critical requests for the sake of brevity.
-    const topCriticalRequests = criticalRequests
+    const CRITICAL_SELF_TIME_MS = 200;
+    const criticalRequests = waterfall
+        .filter((a) => a.selfTime > CRITICAL_SELF_TIME_MS)
         .sort((a, b) => b.selfTime - a.selfTime)
+        // Only show the top critical requests for the sake of brevity.
         .slice(0, 5);
     const blockedTime =
-      topCriticalRequests.reduce((sum, r) => sum + r.selfTime, 0) / 1000;
+      // @ts-ignore param types not inferred.
+      criticalRequests.reduce((sum, r) => sum + r.selfTime, 0) / 1000;
     const failed = criticalRequests.length > 3 || blockedTime > 0.5;
     return {
-      numericValue: topCriticalRequests.length,
+      numericValue: criticalRequests.length,
       score: failed ? 0 : 1,
       displayValue: failed ? str_(UIStrings.displayValue, {blockedTime}) : '',
-      details: AdRequestCriticalPath.makeTableDetails(
-        HEADINGS, topCriticalRequests),
+      details:
+        CriticalBlockingRequests.makeTableDetails(HEADINGS, criticalRequests),
     };
   }
 }
