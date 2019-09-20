8bab75249e848f199b08676e5a744aaa6fddbb8e
Jon Keller <jonkeller@users.noreply.github.com>
2019-09-20 17:08:51 -0400

    Enable first ad render audit for AdSense (#126)
    * Implements first ad render time audit for AdSense
    
    * Implements first ad render time audit for AdSense
    
    * Removes unneeded if construct

--- lighthouse-plugin-publisher-ads/audits/first-ad-render.js
+++ lighthouse-plugin-publisher-ads/audits/first-ad-render.js
@@ -16,7 +16,6 @@ const ComputedAdRenderTime = require('../computed/ad-render-time');
 const i18n = require('lighthouse/lighthouse-core/lib/i18n/i18n');
 const {auditNotApplicable, runWarning} = require('../messages/common-strings');
 const {Audit} = require('lighthouse');
-const {isAdIframe, isGptIframe} = require('../utils/resource-classification');
 
 const UIStrings = {
   title: 'Latency of first ad render',
@@ -71,12 +70,7 @@ class FirstAdRender extends Audit {
     const {timing} = await ComputedAdRenderTime.request(metricData, context);
 
     if (!(timing > 0)) { // Handle NaN, etc.
-      // Currently only GPT ads are supported by this audit.
-      const nonGptAdSlots = artifacts.IFrameElements.filter(
-        (iframe) => isAdIframe(iframe) && !isGptIframe(iframe));
-      if (nonGptAdSlots.length === 0) {
-        context.LighthouseRunWarnings.push(runWarning.NoAdRendered);
-      }
+      context.LighthouseRunWarnings.push(runWarning.NoAdRendered);
       return auditNotApplicable.NoAdRendered;
     }
 
