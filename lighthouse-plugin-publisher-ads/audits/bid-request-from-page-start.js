e80d91ae117bed57510273284205bd5ac1269a55
Jon Burger <jburger424@users.noreply.github.com>
2019-11-15 11:13:34 -0500

    Include link to documentation for `bid-request-from-page-start`… (#162)

--- lighthouse-plugin-publisher-ads/audits/bid-request-from-page-start.js
+++ lighthouse-plugin-publisher-ads/audits/bid-request-from-page-start.js
@@ -23,7 +23,9 @@ const UIStrings = {
   description: 'This metric measures the elapsed time from the start of page ' +
   'load until the first bid request is made. Delayed bid requests will ' +
   'decrease impressions and viewability, and have a negative impact on ad ' +
-  'revenue.',
+  'revenue. [Learn More](' +
+  'https://developers.google.com/publisher-ads-audits/reference/audits/bid-request-from-page-start' +
+  ').',
   displayValue: '{timeInMs, number, seconds} s',
 };
 
