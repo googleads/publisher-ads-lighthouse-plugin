d8159e85577b7b485569f40cdf0c0a6910819923
Warren Godone Maresca <warrengm@google.com>
2019-12-23 10:37:43 -0500

    Enforce that the bid time metric is less than ad req time

--- lighthouse-plugin-publisher-ads/computed/bid-request-time.js
+++ lighthouse-plugin-publisher-ads/computed/bid-request-time.js
@@ -17,8 +17,8 @@ const AdLanternMetric = require('./ad-lantern-metric');
 const ComputedMetric = require('lighthouse/lighthouse-core/computed/metrics/metric');
 // @ts-ignore
 const makeComputedArtifact = require('lighthouse/lighthouse-core/computed/computed-artifact');
-const {getBidStartTime, getPageStartTime} = require('../utils/network-timing');
-const {isBidRequest} = require('../utils/resource-classification');
+const {getAdStartTime, getBidStartTime, getPageStartTime} = require('../utils/network-timing');
+const {isAdRequest, isBidRequest} = require('../utils/resource-classification');
 
 // @ts-ignore
 // eslint-disable-next-line max-len
@@ -36,6 +36,11 @@ class LanternBidRequestTime extends AdLanternMetric {
     const {nodeTimings} = simulationResult;
     const timeInMs = AdLanternMetric.findNetworkTiming(
       nodeTimings, isBidRequest).startTime;
+    const adRequestTimeMs = AdLanternMetric.findNetworkTiming(
+      nodeTimings, isAdRequest).startTime;
+    if (timeInMs > adRequestTimeMs) {
+      return {timeInMs: -1, nodeTimings};
+    }
     return {timeInMs, nodeTimings};
   }
 }
@@ -67,8 +72,12 @@ class BidRequestTime extends ComputedMetric {
     const {networkRecords} = data;
     const pageStartTime = getPageStartTime(networkRecords);
     const bidStartTime = getBidStartTime(networkRecords);
+    const adStartTime = getAdStartTime(networkRecords);
+    if (adStartTime < bidStartTime) {
+      return {timing: -1};
+    }
     const bidRequestTimeMs = (bidStartTime - pageStartTime) * 1000;
-    return Promise.resolve({timing: bidRequestTimeMs});
+    return {timing: bidRequestTimeMs};
   }
 }
 
