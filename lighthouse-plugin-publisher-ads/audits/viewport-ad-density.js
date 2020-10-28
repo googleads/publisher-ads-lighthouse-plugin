941079c2e852d4bc96ff7db56a10d687b3acb5b4
Warren Maresca <warrengm@google.com>
2020-10-28 13:34:08 -0400

    Measure ad density vertically

--- lighthouse-plugin-publisher-ads/audits/viewport-ad-density.js
+++ lighthouse-plugin-publisher-ads/audits/viewport-ad-density.js
@@ -19,16 +19,16 @@ const {boxViewableArea} = require('../utils/geometry');
 const {isAdIframe} = require('../utils/resource-classification');
 
 const UIStrings = {
-  title: 'Ad density in initial viewport is within recommended range',
-  failureTitle: 'Reduce ad density in initial viewport',
-  description: 'The ads-to-content ratio inside the viewport can have an ' +
+  title: 'Ad density in the document is within recommended range',
+  failureTitle: 'Reduce ad density in the document',
+  description: 'The ads-to-content ratio inside the document can have an ' +
   'impact on user experience and ultimately user retention. The Better Ads ' +
   'Standard [recommends having an ad density below 30%]' +
   '(https://www.betterads.org/mobile-ad-density-higher-than-30/). ' +
   '[Learn more](' +
   'https://developers.google.com/publisher-ads-audits/reference/audits/viewport-ad-density' +
   ').',
-  displayValue: '{adDensity, number, percent} covered by ads',
+  displayValue: '{adDensity, number, percent} ad density',
 };
 
 const str_ = i18n.createMessageInstanceIdFn(__filename, UIStrings);
@@ -64,22 +64,24 @@ class ViewportAdDensity extends Audit {
       return auditNotApplicable.NoVisibleSlots;
     }
 
-    const adArea = slots.reduce((sum, slot) =>
-      sum + boxViewableArea(slot.clientRect, viewport), 0);
-
-    const viewArea = viewport.innerWidth * viewport.innerHeight;
-
-    if (viewArea <= 0) {
+    if (viewport.innerHeight <= 0) {
       throw new Error(auditError.ViewportAreaZero);
     }
-    if (adArea > viewArea) {
-      throw new Error(auditError.AreaLargerThanViewport);
-    }
-    const adDensity = adArea / viewArea;
+
+    // We measure document length based on the bottom ad so that it isn't skewed
+    // by lazy loading.
+    const adsBottom =
+      Math.max(...slots.map(s => s.clientRect.top + s.clientRect.height / 2));
+    const documentLength = adsBottom + viewport.innerHeight;
+
+    const adHeights =
+      slots.map(s => s.clientRect.height).reduce((a, b) => a + b, 0);
+
+    const adDensity = adHeights / documentLength;
     const score = adDensity > 0.3 ? 0 : 1;
     return {
       score,
-      numericValue: adArea / viewArea,
+      numericValue: adDensity,
       numericUnit: 'unitless',
       displayValue: str_(UIStrings.displayValue, {adDensity}),
     };
